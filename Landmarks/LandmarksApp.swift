1bae4e88c608bc8c81a441771e432f23361d2d9d
Aurelian Rosca <aurelian@roscasend.com>
2022-05-04 01:02:01 +0300

    Basic version of Landmarks project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by  Aurelian on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
